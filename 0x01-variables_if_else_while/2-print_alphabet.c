8837e1b82bf090b6615bf9967407f527ec1de77d
amineboussakssou2 <amineboussakssou2@gmail.com>
2023-08-11 08:06:55 -0700

    test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char amine;
+
+for (amine = 'a'; amine <= 'z'; amine++)
+	putchar(amine);
+
+putchar('\n');
+
+return (0);
+}
