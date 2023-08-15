007a71a002463c4e665d034f9c3e9404c5a84f62
amineboussakssou2 <amineboussakssou2@gmail.com>
2023-08-15 07:04:05 -0700

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point Print numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+	{
+
+	char write[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(write[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
