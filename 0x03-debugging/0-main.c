5a6737c5b7f8e8ca32c4cadb020fbe92b647fc07
amineboussakssou2 <amineboussakssou2@gmail.com>
2023-08-16 05:58:17 -0700

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
-* main - function prints if int + or -
+* main - Test function for positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-	int i = 0;
-
-	positive_or_negative(i);
-
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
