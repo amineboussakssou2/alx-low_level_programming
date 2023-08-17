0bff82752c0df8bc25f923eb4222787a84b682f7
amineboussakssou2 <amineboussakssou2@gmail.com>
2023-08-17 11:52:02 -0700

    est

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - proto function
+ *
+ * @a: First number
+ * @b: 2nd number
+ *
+ * Return: Multip a and b
+ */
+
+int mul(int a, int b)
+
+{
+	int multip;
+
+	multip = a * b;
+	return (multip);
+}
