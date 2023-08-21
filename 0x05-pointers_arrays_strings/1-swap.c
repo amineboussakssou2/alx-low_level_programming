e142f357927a84b25ff87975fae7a5ac5113aae1
amineboussakssou2 <amineboussakssou2@gmail.com>
2023-08-21 06:19:43 -0700

    test

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * swap_int - check the code
+ *@a: pointer *a
+ *@b: pointer *b
+ * Return: bo return
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
