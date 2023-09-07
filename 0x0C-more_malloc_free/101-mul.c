73c62be4781eb61a05820d8da4ebda68b488af4a
amineboussakssou2 <amineboussakssou2@gmail.com>
2023-09-07 07:22:48 -0700

    test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,90 +1,101 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * print_error - Prints an error message.
- */
-void print_error(void)
-{
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-}
-
-/**
- * is_valid_digit - Checks if a character is a valid digit.
- * @c: The character to check.
- * Return: 1 if it's a digit, 0 otherwise.
- */
-int is_valid_digit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
- * print_result - Prints the result.
- * @result: The result to print.
- */
-void print_result(long result)
-{
-	char buffer[20];
-	int len = 0;
-	int i;
-
-	if (result == 0)
-		_putchar('0');
-	else
-	{
-		while (result)
-		{
-			buffer[len++] = result % 10 + '0';
-			result /= 10;
-		}
-		for (i = len - 1; i >= 0; i--)
-			_putchar(buffer[i]);
-	}
-
-	_putchar('\n');
-}
-
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array of strings representing the arguments
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: 0 on success, 98 on incorrect arguments, 98 on non-digit input
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	long result;
-	int i;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		print_error();
-		return (98);
-	}
-
-	for (i = 1; i < 3; i++)
-	{
-		char *num = argv[i];
-
-		while (*num)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (!is_valid_digit(*num))
-			{
-				print_error();
-				return (98);
-			}
-			num++;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	result = atoll(argv[1]) * atoll(argv[2]);
-	print_result(result);
-
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
